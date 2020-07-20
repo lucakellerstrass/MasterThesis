21722dad52353316563cccff5f95a59966963b65
lucak <lucak@Kellerstrass-Surface-Pro7.fritz.box>
2020-07-20 20:39:36 +0200

    added the hsqldb.jar in lib

---
+++ Master-Thesis_Luca_Kellerstrass_Copy/src/main/kellerstrass/ModelCalibration/DataSetCalibration.java
@@ -0,0 +1,20 @@
+package kellerstrass.ModelCalibration;
+
+/**The class DataSetCalibration is build to calibrate different models using sets of calibration data.
+ * <br>
+ * A possible operation area is the calibration to a set of different days, e.g. a year of information.
+ * 
+ * @author lucak
+ *
+ */
+public class DataSetCalibration {
+
+	String dataDays; 
+	
+	public DataSetCalibration(String[] dataDays) {
+		
+		this.dataDays = 
+		
+	}
+	
+}
